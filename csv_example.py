e14007306b7b8124809ae464ddf5e9b731669697
Shaun Koon <shaun.koon@hotmail.com>
2019-06-05 11:10:21 +0100

    Corrected tests

--- csv_example.py
+++ csv_example.py
@@ -12,21 +12,22 @@ import csv
 #         print(row[-1])
 
 
-# with open('user_details.csv', newline='') as csv_file:
-#     csvreader = csv.reader(csv_file, delimiter=',')
-#     iterable = iter(csvreader)
-#     print(iterable)
-#     haeders = next(iterable)
-#     for row in iterable:
-#         print(row)
+with open('user_details.csv', newline='') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=',')
+    iterable = iter(csvreader)
+    #print(iterable)
+    next(iterable)
+
+    for row in iterable:
+        print(row)
 
 # list
 
-with open('user_details.csv', newline='') as csv_file:
-    csvreader = csv.reader(csv_file, delimiter=',')
-    list_list = list(csvreader)
-    print(type(list_list))
-    print(len(list_list))
-    # for row in list(csvreader):
-    #     print(row)
-    #     print('helloeoe')
+# with open('user_details.csv', newline='') as csv_file:
+#     csvreader = csv.reader(csv_file, delimiter=',')
+#     list_list = list(csvreader)
+#     print(type(list_list))
+#     print(len(list_list))
+#     # for row in list(csvreader):
+#     #     print(row)
+#     #     print('helloeoe')
--- imbd_own criteria.py
+++ imbd_own criteria.py
@@ -9,6 +9,34 @@ with open('imdb_title.csv', newline='') as csvfile:
 # Get the data and remove originaltitle, isAdult, endYear
 
 
+# def transform_user_details(csv_full_name):
+#     newuser_data = []
+#     try:
+#         with open(csv_full_name, newline='') as csv_file:
+#             user_details_csv = csv.reader(csv_file, delimiter=',')
+#             iterable = iter(user_details_csv)
+#             next(iterable)
+#             for rowlist in iterable:
+#                 transformed_row = []
+#                 if int(rowlist[6]) == :
+#                     transformed_row.append(rowlist[0].capitalize())  # titleType
+#                     transformed_row.append(rowlist[2].capitalize())  # originalTitle
+#                     transformed_row.append(rowlist[4])               # startYear
+#                     transformed_row.append(rowlist[6])               # runtimeMinutes
+#                     transformed_row.append(rowlist[7].capitalize())  # genres
+#                     newuser_data.append(transformed_row)
+#             return newuser_data
+#     except FileNotFoundError as ermsg:
+#         print("Excuuuse me! Please make sure a file exists")
+#         print(ermsg)
+#     finally:
+#         print("execution doneee")
+#
+#
+# new_trans_data = transform_user_details('imdb_title.csv')
+# print(new_trans_data)
+
+
 def transform_user_details(csv_full_name):
     newuser_data = []
     try:
@@ -17,13 +45,15 @@ def transform_user_details(csv_full_name):
 
             for rowlist in user_details_csv:
                 transformed_row = []
-                if wordnet.synsets(rowlist[2]) == False: #and int(rowlist[6]) >= 2015:
-                    transformed_row.append(rowlist[0].capitalize())  # titleType
-                    transformed_row.append(rowlist[2].capitalize())  # originalTitle
-                    transformed_row.append(rowlist[4])               # startYear
-                    transformed_row.append(rowlist[6])               # runtimeMinutes
-                    transformed_row.append(rowlist[7].capitalize())  # genres
-                    newuser_data.append(transformed_row)
+                if "Horror" in rowlist[7] and int(rowlist[4]) >= 2010:
+                    if "N" not in rowlist[6]:
+                        if int(rowlist[6]) < 1500:
+                            transformed_row.append(rowlist[0].capitalize())  # titleType
+                            transformed_row.append(rowlist[2].capitalize())  # originalTitle
+                            transformed_row.append(rowlist[4])               # startYear
+                            transformed_row.append(rowlist[6])               # runtimeMinutes
+                            transformed_row.append(rowlist[7].capitalize())  # genres
+                            newuser_data.append(transformed_row)
             return newuser_data
     except FileNotFoundError as ermsg:
         print("Excuuuse me! Please make sure a file exists")
@@ -57,4 +87,4 @@ def create_new_csv_user_data(transformed_data, new_user_file_name):
         print("execution doneee")
 
 
-#create_new_csv_user_data(ne_trans_data, 'imdb_transformed_csv.csv')
+create_new_csv_user_data(new_trans_data, 'imdb_comedy_2018+_csv.csv')
